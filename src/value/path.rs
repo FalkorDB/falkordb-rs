666b5171547f496111a639b55eaaa4d55a2463f6
Emily Matheys <emilymatt96@gmail.com>
2024-06-20 14:55:56 +0300

    fix: Use the MIT license, as intended originally

--- src/value/path.rs
+++ src/value/path.rs
@@ -1,6 +1,6 @@
 /*
  * Copyright FalkorDB Ltd. 2023 - present
- * Licensed under the Server Side Public License v1 (SSPLv1).
+ * Licensed under the MIT License.
  */
 
 use crate::{parser::redis_value_as_vec, Edge, FalkorDBError, FalkorResult, GraphSchema, Node};
